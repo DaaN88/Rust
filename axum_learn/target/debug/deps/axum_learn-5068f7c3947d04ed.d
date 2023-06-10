5ba409c30b89d9aad9b215ee35a7d4bd461547d4
Антон Шведов <ashvedov@simtechdev.org>
2023-06-10 16:00:08 +0400

    delete target directories; added /target dir's in gitignore

--- axum_learn/target/debug/deps/axum_learn-5068f7c3947d04ed.d
+++
@@ -1,5 +0,0 @@
-/Users/antonsvedov/Programming/Rust/axum_learn/target/debug/deps/axum_learn-5068f7c3947d04ed.rmeta: src/main.rs
-
-/Users/antonsvedov/Programming/Rust/axum_learn/target/debug/deps/axum_learn-5068f7c3947d04ed.d: src/main.rs
-
-src/main.rs:
--- axum_learn/target/debug/deps/axum_learn-9ad9c5f40058afc0.d
+++
@@ -1,5 +0,0 @@
-/Users/antonsvedov/Programming/Rust/axum_learn/target/debug/deps/axum_learn-9ad9c5f40058afc0.rmeta: src/main.rs
-
-/Users/antonsvedov/Programming/Rust/axum_learn/target/debug/deps/axum_learn-9ad9c5f40058afc0.d: src/main.rs
-
-src/main.rs:
